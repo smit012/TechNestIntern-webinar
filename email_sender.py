6a15fbc094078c85d4f9531c91b14328f336ad8a
Surani Smit <111040112+smit012@users.noreply.github.com>
2025-07-13 09:54:11 +0530

    email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -74,7 +74,7 @@ html_body = """
       🔗<a href="https://www.linkedin.com/company/technestintern/about/" target="_blank" style="color: #007bff;">Follow on LinkedIn</a>
       </p>
 
-      <img src="https://i.ibb.co/Y4vZ2819/logo.png" alt="TechNest Logo" width="140" style="margin-top: 20px;">
+      <img src="https://ibb.co/tTmKYz4f" alt="TechNest Logo" width="140" style="margin-top: 20px;">
 
     </div>
   </body>
